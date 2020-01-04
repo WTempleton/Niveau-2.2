13e758dfed9f29acc82437b809b91f4daf428c30
WilliamTempleton <william.templeton@centrale-marseille.fr>
2020-01-04 17:01:06 +0100

    Ajout du code html, css, typescript

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 "use strict";
+function inserer(num) {
+    return document.getElementsByClassName("resultat")[0].value = document.getElementsByClassName("resultat")[0].value + num;
+}
+function calcul() {
+    return document.getElementsByClassName("resultat")[0].value = eval(document.getElementsByClassName("resultat")[0].value);
+}
+function del() {
+    return document.getElementsByClassName("resultat")[0].value = "";
+}
